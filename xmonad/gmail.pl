9e28878d66cf61b21826ef168a086e209bd64433
Terje Runde <terje.runde@gmail.com>
2013-10-24 18:02:40 +0200

    Gmail script now has better error handling (supports disconnectivity).

--- xmonad/gmail.pl
+++ xmonad/gmail.pl
@@ -5,6 +5,7 @@ use warnings;
 
 use WWW::Curl::Easy;
 use XML::Simple;
+use IO::Interface::Simple;
 
 die "Usage: $0 <username> <gpg file>\n" unless scalar @ARGV == 2;
 
@@ -18,17 +19,29 @@ open my $fh, '-|', "gpg -q --decrypt $pw_path"
 my $pw = <$fh>;
 
 # Do some curling
-my $curl = WWW::Curl::Easy->new;
+my $curl = WWW::Curl::Easy->new();
 
 my $response;
 $curl->setopt(CURLOPT_WRITEDATA,\$response);
 $curl->setopt(CURLOPT_URL, $gmail_url);
 $curl->setopt(CURLOPT_USERPWD, "$user:$pw");
-$curl->perform;
 
-my $xml = XML::Simple->new();
-my $data = $xml->XMLin($response);
+$curl->setopt(CURLOPT_NOSIGNAL, 1);
+$curl->setopt(CURLOPT_TIMEOUT_MS, 1000);
+$curl->setopt(CURLOPT_CONNECTTIMEOUT, 1);
 
-my $new_mails = $data->{fullcount};
+my @ips = grep { $_->is_running
+    and defined $_->address
+    and $_->address ne '127.0.0.1'
+} IO::Interface::Simple->interfaces;
 
-printf "Mail: %d", $new_mails;
+my $new_mails = 'x';
+if (@ips) {
+    if (not $curl->perform) {
+        my $xml = XML::Simple->new();
+        my $data = $xml->XMLin($response);
+        $new_mails = $data->{fullcount};
+    }
+}
+
+printf "Mail: %s", $new_mails;
